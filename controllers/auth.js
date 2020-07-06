39188f03b484ca75ca3a3119e228abdfcfb7238a
M.Hunain Khalid <muhammad.khalid@10pearls.com>
2020-07-07 01:33:39 +0500

    Error handling & File upload and download

--- controllers/auth.js
+++ controllers/auth.js
@@ -115,7 +115,11 @@ exports.postLogin = (req, res, next) => {
           res.redirect("/login");
         });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postSignup = (req, res, next) => {
@@ -155,7 +159,11 @@ exports.postSignup = (req, res, next) => {
           subject: "Signup succeeded",
           html: "<h1>You successfully signed up!</h1>",
         })
-        .catch((err) => console.log(err));
+        .catch((err) => {
+          const error = new Error(err);
+          error.httpStatusCode = 500;
+          return next(error);
+        });
     });
 };
 
@@ -209,7 +217,11 @@ exports.postReset = (req, res, next) => {
           `,
         });
       })
-      .catch((err) => consloge.log(err));
+      .catch((err) => {
+        const error = new Error(err);
+        error.httpStatusCode = 500;
+        return next(error);
+      });
   });
 };
 
@@ -231,7 +243,11 @@ exports.getNewPassword = (req, res, next) => {
         passwordToken: token,
       });
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
 
 exports.postNewPassword = (req, res, next) => {
@@ -258,5 +274,9 @@ exports.postNewPassword = (req, res, next) => {
     .then((result) => {
       res.redirect("/login");
     })
-    .catch((err) => console.log(err));
+    .catch((err) => {
+      const error = new Error(err);
+      error.httpStatusCode = 500;
+      return next(error);
+    });
 };
